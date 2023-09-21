f3201d3bdb63ead72b7eaeb0b192e1129af349c8
rafa30052003 <rafalucenavalle9@gmail.com>
2023-09-21 14:42:39 +0200

    dto

--- src/model/dto/Customer.java
+++ src/model/dto/Customer.java
@@ -1,4 +1,72 @@
 package model.dto;
 
+import java.util.Objects;
+
 public class Customer {
+   private String dni;
+   private String nombre;
+   private String direccion;
+   private String tlf;
+
+    public Customer() {
+    }
+
+    public Customer(String dni, String nombre, String direccion, String tlf) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.tlf = tlf;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+        return Objects.equals(dni, customer.dni) && Objects.equals(nombre, customer.nombre) && Objects.equals(direccion, customer.direccion) && Objects.equals(tlf, customer.tlf);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", tlf='" + tlf + '\'' +
+                '}';
+    }
+
 }
--- src/model/dto/Order.java
+++ src/model/dto/Order.java
@@ -1,4 +1,58 @@
 package model.dto;
 
+import java.util.Objects;
+
 public class Order {
+    private int cod;
+    private String nombre;
+    private String proveedor;
+
+    public Order() {
+    }
+
+    public Order(int cod, String nombre, String proveedor) {
+        this.cod = cod;
+        this.nombre = nombre;
+        this.proveedor = proveedor;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order order)) return false;
+        return cod == order.cod && Objects.equals(nombre, order.nombre) && Objects.equals(proveedor, order.proveedor);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "cod=" + cod +
+                ", nombre='" + nombre + '\'' +
+                ", proveedor='" + proveedor + '\'' +
+                '}';
+    }
 }
