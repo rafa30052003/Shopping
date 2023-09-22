8de2c70b408396c92a06eee3c0183953a1c24ac2
fcar16 <fcarlop2710@g.educaand.es>
2023-09-22 10:10:36 +0200

    Vistas terminadas

--- src/view/PrincipalView.java
+++ src/view/PrincipalView.java
@@ -1,4 +1,63 @@
 package view;
 
 public class PrincipalView {
+	
+	
+	public void showManagementMenu() {
+        System.out.println("                             ");
+        System.out.println(" ___________________________ ");
+        System.out.println("|                           |");
+        System.out.println("|    GESTIÓN DE PEDIDOS     |");
+        System.out.println("|                           |");
+        System.out.println("| [0]-> Salir               |");
+        System.out.println("| [1]-> Añadir Pedido	    |");
+        System.out.println("| [2]-> Ver Pedidos         |");
+        System.out.println("| [3]-> Editar Pedido       |");
+        System.out.println("| [4]-> Eliminar Pedido     |");
+        System.out.println("|                           |");
+        System.out.println("|___________________________|");
+        System.out.println("                             ");
+	}
+	
+	public void addPedido() {
+
+        System.out.println("                             ");
+        System.out.println(" __________________________");
+        System.out.println("|                          |");
+        System.out.println("|      AÑADIR PEDIDO       |");
+        System.out.println("|__________________________|");
+        System.out.println("                             ");
+	}
+        
+    public void deletePedido() {
+
+            System.out.println("                             ");
+            System.out.println(" __________________________");
+            System.out.println("|                          |");
+            System.out.println("|     ELIMINAR PEDIDO      |");
+            System.out.println("|__________________________|");
+            System.out.println("                             ");
+    }
+    
+	public void Pedidos() {
+
+        System.out.println("                             ");
+        System.out.println(" __________________________");
+        System.out.println("|                          |");
+        System.out.println("|         PEDIDOS          |");
+        System.out.println("|__________________________|");
+        System.out.println("                             ");
+	}
+	public void EditPedidos() {
+
+        System.out.println("                             ");
+        System.out.println(" __________________________");
+        System.out.println("|                          |");
+        System.out.println("|       EDITAR PEDIDO      |");
+        System.out.println("|__________________________|");
+        System.out.println("                             ");
+	}
+
+
+
 }
